a655c0678b57f69284a301fb4b7f31578f1d9051
siigmaman <aedudkina@edu.hse.ru>
2025-07-26 20:28:19 +0000

    analysePerson

--- helpers.cpp
+++ helpers.cpp
@@ -1,7 +1,10 @@
 #include "psycho.hpp"
 
+#include <iostream>
 #include <cctype>
 #include <sstream>
+#include <algorithm>
+#include <utility>
 
 namespace Psycho {
     std::vector<std::string> PsychologicalAnalyzerBot::parseMessage(const std::string& text) {
@@ -28,4 +31,58 @@ namespace Psycho {
 
         return words;
     }
+
+    std::string PsychologicalAnalyzerBot::analyzePerson(const std::string& name) {
+        auto userId = tendency.find(name);
+
+        if (userId == tendency.end()) {
+            return "Нет данных для" + name;
+        }
+
+        const auto& userPsycho = userId->second;
+
+        if (userPsycho.empty()) {
+            return "Недостаточно информации для" + name;
+        }
+
+        std::vector<std::pair<std::string, int>> sorted(userPsycho.begin(), userPsycho.end());
+
+        std::sort(sorted.begin(), sorted.end(), [](const auto& a, const auto& b) {
+            return a.second > b.second;
+        });
+        
+        std::vector<std::string> resultPsycho;
+
+        int max1 = sorted[0].second;
+        int max2 = sorted[1].second;
+
+        for (const auto& [type, score] : sorted) {
+            if (score == max1) {
+                resultPsycho.push_back(type);
+            }
+        }
+
+        if ((max1 - max2) < (max1 / 10)) {
+            for (const auto& [type, score] : sorted) {
+                if (score == max2) {
+                    resultPsycho.push_back(type);
+                }
+            }
+        } 
+
+        std::string result;
+        int n = static_cast<int> resultPsycho.size();
+
+        for (int i = 0; i < n; ++i) {
+            result += resultPsycho[i];
+            if (i + 2 == n) {
+                result += " и ";
+            } else if (i + 1 < n) {
+                result += ", ";
+            }
+        }
+
+        return result;
+    } 
+
 }
